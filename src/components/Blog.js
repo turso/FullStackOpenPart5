3a427c628f2c7b1321591d8e88cc194ce2240eea
Oliver Rikkilä <turso@olen.to>
2018-11-20 22:10:14 +0200

    tehtava 5.6 valicommit

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
+import '../StyleZ.css';
 
-export default Blog
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    console.log('PAINETTIIN');
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+
+  render() {
+    console.log('YKS BLOGI', this.props.blog);
+    return (
+      <div className="blog" onClick={this.handleClick}>
+        {this.props.blog.title} {this.props.blog.author}
+        {this.state.clicked && (
+          <div>
+            <div>{this.props.blog.url}</div>
+            <div>{this.props.blog.likes} likes</div>
+            {this.props.blog.user && <div> added by {this.props.blog.user.username} </div>}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Blog;

da6dd89263e67fd1480ff171f34dc86bad8698fd
Oliver Rikkilä <turso@olen.to>
2018-11-21 12:53:06 +0200

    tehtava 5.6 valmis

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,21 +10,22 @@ class Blog extends React.Component {
   }
 
   handleClick = () => {
-    console.log('PAINETTIIN');
     this.setState({
       clicked: !this.state.clicked
     });
   };
 
   render() {
-    console.log('YKS BLOGI', this.props.blog);
+    // console.log('YKS BLOGI', this.props.blog);
     return (
       <div className="blog" onClick={this.handleClick}>
         {this.props.blog.title} {this.props.blog.author}
         {this.state.clicked && (
           <div>
             <div>{this.props.blog.url}</div>
-            <div>{this.props.blog.likes} likes</div>
+            <div>
+              {this.props.blog.likes} likes <button type="button">like</button>
+            </div>
             {this.props.blog.user && <div> added by {this.props.blog.user.username} </div>}
           </div>
         )}

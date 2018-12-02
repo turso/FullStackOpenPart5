01e09b8bfb9e1dfe2993babbed4a0d1f7dc1792f
Oliver Rikkilä <turso@olen.to>
2018-12-02 20:31:20 +0200

    tehtava 5.17

--- src/App.js
+++ src/App.js
@@ -1,165 +1,83 @@
 import React from 'react';
-import Blog from './components/Blog';
-import Notification from './components/Notification';
-import blogService from './services/blogs';
-import loginService from './services/login';
-import BlogForm from './components/BlogForm';
-import LoginForm from './components/LoginForm';
-import Togglable from './components/Toggable';
-import './StyleZ.css';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import counterReducer from './reducers/counterReducer';
 
+const store = createStore(counterReducer);
+
+const Statistiikka = () => {
+  const state = store.getState();
+  const good = state.good;
+  const ok = state.ok;
+  const bad = state.bad;
+  const total = good + ok + bad;
+  const average = (good - bad) / total;
+  const positive = Math.round(good / total * 100);
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>Statistiikka</h2>
+        <div>Ei yhtään palautetta annettu</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Statistiikka</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>Hyvä</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>Ok</td>
+            <td>{ok}</td>
+          </tr>
+          <tr>
+            <td>Huono</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>Keskiarvo</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>Positiivisia</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blogs: [],
-      blog: '',
-      username: '',
-      password: '',
-      user: null,
-      error: null,
-      notification: null
-    };
-  }
-
-  componentDidMount() {
-    blogService.getAll().then(blogs => this.setState({ blogs }));
-
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser');
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      this.setState({ user });
-    }
-  }
-
-  refreshBlogList() {
-    blogService.getAll().then(blogs => this.setState({ blogs }));
-  }
-
-  updateNotifications = notification => {
-    this.setState({
-      notification: notification
-    });
-    setTimeout(() => {
-      this.setState({ notification: null });
-    }, 6000);
-  };
-
-  updateErrors = error => {
-    this.setState({
-      error: error
-    });
-    setTimeout(() => {
-      this.setState({ error: null });
-    }, 6000);
-  };
-
-  login = async event => {
+  click = action => event => {
     event.preventDefault();
-    try {
-      const user = await loginService.login({
-        username: this.state.username,
-        password: this.state.password
-      });
-
-      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user));
-      blogService.setToken(user.token);
-      this.setState({ username: '', password: '', user });
-    } catch (exception) {
-      this.setState({
-        error: 'wrong username or password'
-      });
-      setTimeout(() => {
-        this.setState({ error: null });
-      }, 6000);
-    }
-  };
-
-  logout = event => {
-    event.preventDefault();
-    window.localStorage.clear();
-    this.setState({ user: null });
-  };
-
-  handleFieldChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  updateLikes = async blog => {
-    console.log(blog);
-    const updatedBlog = { ...blog, likes: blog.likes + 1 };
-
-    await blogService.update(blog._id, updatedBlog);
-    this.refreshBlogList();
-  };
-
-  deleteBlog = async blog => {
-    console.log(blog);
-    if (window.confirm(`Do you really want to delete ${blog.title} by ${blog.author} ?`)) {
-      await blogService.deleteOne(blog._id);
-      const message = `${blog.title} by ${blog.author} has been deleted`;
-      this.updateNotifications(message);
-      this.refreshBlogList();
-    }
+    store.dispatch({ type: action });
   };
 
   render() {
-    const loginForm = () => (
-      <Togglable buttonLabel="login">
-        <LoginForm
-          visible={this.state.visible}
-          username={this.state.username}
-          password={this.state.password}
-          handleChange={this.handleFieldChange}
-          handleSubmit={this.login}
-        />
-      </Togglable>
-    );
-
-    const blogForm = () => {
-      return (
-        <div>
-          <div className="login-second-title">
-            <div>{this.state.user.name} logged in</div>
-            <button className="logout-button" type="button" onClick={this.logout}>
-              logout
-            </button>
-          </div>{' '}
-          <Togglable buttonLabel="create new blog" ref={component => (this.BlogForm = component)}>
-            <BlogForm
-              refreshBlogList={this.refreshBlogList.bind(this)}
-              updateNotifications={this.updateNotifications.bind(this)}
-              updateErrors={this.updateErrors.bind(this)}
-              toggleVisibility={() => this.BlogForm.toggleVisibility()}
-            />
-          </Togglable>
-          {this.state.blogs
-            .sort((blog, mostLikes) => {
-              return mostLikes.likes - blog.likes;
-            })
-            .map(blog => (
-              <Blog
-                updateLikes={this.updateLikes}
-                deleteBlog={this.deleteBlog}
-                key={blog._id}
-                blog={blog}
-                user={this.state.user}
-              />
-            ))}
-        </div>
-      );
-    };
-
     return (
       <div>
-        <h2 className="h2">blogs</h2>
-
-        <Notification error={this.state.error} notification={this.state.notification} />
-
-        {this.state.user === null ? loginForm() : <div>{blogForm()}</div>}
+        <h2>Anna palautetta</h2>
+        <button onClick={this.click('GOOD')}>hyvä</button>
+        <button onClick={this.click('OK')}>ok</button>
+        <button onClick={this.click('BAD')}>huono</button>
+        <Statistiikka />
+        <button onClick={this.click('ZERO')}>nollaa tilasto</button>
       </div>
     );
   }
 }
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
 
 export default App;
